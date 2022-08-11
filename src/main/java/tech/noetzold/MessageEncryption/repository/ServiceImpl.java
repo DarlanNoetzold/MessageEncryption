f47b40543d96261708ee66293f9b60f893b05bfb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-11 10:55:16 -0300

    Update ServiceImpl.java

--- src/main/java/tech/noetzold/MessageEncryption/repository/ServiceImpl.java
+++ src/main/java/tech/noetzold/MessageEncryption/repository/ServiceImpl.java
@@ -1,4 +1,13 @@
 package tech.noetzold.MessageEncryption.repository;
 
+import tech.noetzold.MessageEncryption.model.Message;
+
+import java.util.List;
+
 public interface ServiceImpl {
+
+    List<Message> getOneById(Long id);
+    List<Message> getAll();
+    Message getOne(Long id);
+    void save(Message message);
 }
