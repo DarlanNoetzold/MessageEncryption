a35c2ddf57fd6dd2f077dfbc526991e6eb5b08f9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-11 10:55:06 -0300

    Update Message.java

--- src/main/java/tech/noetzold/MessageEncryption/model/Message.java
+++ src/main/java/tech/noetzold/MessageEncryption/model/Message.java
@@ -1,4 +1,32 @@
 package tech.noetzold.MessageEncryption.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
