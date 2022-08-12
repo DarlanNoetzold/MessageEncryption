ebb505e8daa276f5c5f18505c6e2adc1520fbee2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-12 09:12:50 -0300

    Update BitcoinController.java

--- src/main/java/tech/noetzold/MessageEncryption/controller/BitcoinController.java
+++ src/main/java/tech/noetzold/MessageEncryption/controller/BitcoinController.java
@@ -1,4 +1,30 @@
 package tech.noetzold.MessageEncryption.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Controller
 public class BitcoinController {
+
+    @GetMapping("/show")
+    public String showHomePage(){
+        return "index.html";
+    }
+
+    public static String sha256(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+        return convertByteArrayToHexString(hash);
+    }
+
+    public static String convertByteArrayToHexString(byte[] arrayBytes){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte i: arrayBytes) stringBuilder.append(Integer.toString((i & 0xff) + 0x100, 16).substring(1));
+        return stringBuilder.toString();
+    }
 }
