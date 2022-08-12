590a549aac9459cfd398713d7d83cd5e60e56805
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-12 09:12:54 -0300

    Update ServiceImpIpm.java

--- src/main/java/tech/noetzold/MessageEncryption/repository/ServiceImpIpm.java
+++ src/main/java/tech/noetzold/MessageEncryption/repository/ServiceImpIpm.java
@@ -1,4 +1,28 @@
 package tech.noetzold.MessageEncryption.repository;
 
-public class ServiceImpIpm {
+import tech.noetzold.MessageEncryption.model.Message;
+
+import java.util.List;
+
+public class ServiceImpIpm implements ServiceImpl{
+
+    @Override
+    public List<Message> getOneById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Message> getAll() {
+        return null;
+    }
+
+    @Override
+    public Message getOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Message message) {
+
+    }
 }
